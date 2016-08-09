1c690deb6389e9e143bbd83b938ccbbab264e767
gorbachevdmitr <gorbachevdmitr@gmail.com>
2016-08-09 14:40:11 +0300

    add portfoliotemplate

---
+++ js/sc.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dmitry on 8/4/16.
+ */
+/* /Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
